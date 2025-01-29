c69a7a126a513701b7ded3ea2df65b1b1c0fea5c
Cataclysmic Code <133720249+CataclysmicCode@users.noreply.github.com>
2025-01-28 17:05:42 -0800

    Add files via upload

--- load.js
+++ load.js
@@ -1,5 +1,21 @@
 
+function loadExternalJS(TARGET_URL){
+    var xhr = window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
+    xhr.open('GET', TARGET_URL, false);
+    xhr.send(null);
+    var code = xhr.responseText;
+    var dScript = document.createElement('script');
+    try {
+        dScript.appendChild( document.createTextNode(parsed) );
+        document.body.appendChild(dScript);
+    } catch(e) {
+        dScript.text = parsed;
+        document.getElementsByTagName('head')[0].appendChild(dScript);
+    }
+    xhr = null;
+}
+
 var names = "cute1 cute2 cute3 cute4 cute5 piano splash splash2 underwater wilhelm".split(" ");
 for(var i = 0; i < names.length; i++){
-    document.write('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/CataclysmicCode/collab_audio@main/js/'+names[i]+'.js"></script>');
+    loadExternalJS("https://cdn.jsdelivr.net/gh/CataclysmicCode/collab_audio@main/js/'+names[i]+'.js");
 }
